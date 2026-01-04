534f0bdb212fde1bd030e53e4bd4e75c49d4a731
adivdeco <sadiv120@gmail.com>
2026-01-04 23:31:14 +0530

    introducing emmbeding to message part

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -7,7 +7,12 @@ const messageSchema = new mongoose.Schema({
     senderAvatar: { type: String }, // Cache avatar for display
     channel: { type: mongoose.Schema.Types.ObjectId, ref: 'Channel' }, // Optional if conversation
     conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' }, // Optional if channel
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+
+    embedding: {
+        type: [Number], // An array of numbers (floats)
+        index: true     // Essential for performance
+    }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
