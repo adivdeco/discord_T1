acd54d3c1e3bd7c01211e0ae12887fc1143c9e4b
Asfar35 <skasfarali5@gmail.com>
2026-01-23 23:48:17 +0530

    add landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import { Layout } from './components/Layout';
-import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/clerk-react";
 import LandingPage from "./components/landing/LandingPage"; 
 
 function App() {
